36d3e5c3e979fdfbe33f1bbd3607e263cee61ae6
Thales Kohki Hasegawa <kohkihasegawa151@gmail.com>
2021-09-25 21:45:30 -0300

    colocando os arquivos no repositório

---
+++ bot.py
@@ -0,0 +1,25 @@
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix=',')
+
+bot.lava_nodes = [
+    {
+        'host': 'lava.link',
+        'port': 80,
+        'rest_uri': f'http://lava.link:80',
+        'identifier': 'MAIN',
+        'password': 'anything',
+        'region': 'singapore'
+    }
+]
+
+@bot.event
+async def on_ready():
+    print('Bot is ready to play music.')
+    bot.load_extension('dismusic')
+    bot.load_extension('dch')
+
+
+
+
+bot.run('ODg4MjU1OTEzNjI5MjE2Nzc5.YUQCyA.WANDT1MsCRn2lhVu5zVzCEY7hDU')
